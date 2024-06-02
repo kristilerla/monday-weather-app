b6fb23534b14ddb8d5a7cde7299033aaa0a10b75
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-06-02 16:30:13 +0200

    added css on weather application

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App"> Weather app
+    <div className="App"> <h1>Kristils Weather app</h1>
       <div className="container">
         <Weather />
         
-        <div>
-      <a href="https://github.com/kristilerla/monday-weather-app">hosted on Github</a>
-    </div>
+        <footer>
+          This project is created by {""}
+          <a href="https://www.linkedin.com/in/kristilerla/">Kristil Erla</a> and is open sourced on {""}
+      <a href="https://github.com/kristilerla/monday-weather-app"> Github</a>
+    </footer>
   
 
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,64 @@
 import React from "react";
+import "./Weather.css";
+
 
 export default function Weather(){
 
     return (
        
-   
-    <form>
-<input type="text" placeholder="Enter a city" />
-<button type="submit">Get weather</button>
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
 
-    </form>
+                    
+                    
+              
+<input type="search"
+placeholder="Enter a city"
+className="form-control" />
+  </div>
+  <div className="col-3">
+
+
+<input type="submit"
+value="search"
+className="btn btn-primary w-100" />
+  </div>
+
+  </div>
+            </form>
+            <h1>New York</h1>
+            <ul>
+                <li>
+                    Wednesday 07:00
+                </li>
+                <li>Mostly cloudy</li>
+            </ul>
+           <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy">
+
+                </img>
+                6 °C
+
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>
+                        precipitation: 15%
+                    </li>
+                    <li>
+                        Humidity: 87 %
+                    </li>
+                    <li>Wind: 5 km/h</li>
+                </ul>
+            </div>
+        
+           </div>
+            
+          </div>
+    
      );
+    
 }
