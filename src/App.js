fc3606953c60c70481903bea1e41735815b39f31
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-06-02 09:01:44 +0200

    updated link to Github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ export default function App() {
     <div className="App"> Weather app
       <div className="container">
         <Weather />
+        
+        <div>
+      <a href="https://github.com/kristilerla/monday-weather-app">hosted on Github</a>
+    </div>
+  
+
       </div>
     </div>
   );
